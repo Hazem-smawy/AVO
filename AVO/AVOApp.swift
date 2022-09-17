d998073f2173da64a3b5b8c192a53be140940177
hazem-smawy <hazemsmawy@gmail.com>
2022-09-17 17:30:15 +0300

    Initial Commit

---
+++ AVO/AVOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVOApp.swift
+//  AVO
+//
+//  Created by hazem smawy on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct AVOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
